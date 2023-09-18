548c935515027e300d4e88c3d954aeffa099a99d
Cheng Zhang <zhangchengx@gmail.com>
2023-09-18 22:05:33 +0000

    Update settings.py

--- Server-Python-Django/main/settings.py
+++ Server-Python-Django/main/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-q=dn6*sfo*v!z9#5r=-xtqx$+9&b_e5++he+zcqescm50ct1b*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 CORS_ALLOW_ALL_ORIGINS = True
 
